379949d964af043d3230cac1b528e003bd1493bf
nicko <nickomarsellino@gmail.com>
2018-04-04 17:28:32 +0700

    Tinggal Notif di Oreo

--- app/src/main/java/com/example/nickomarsellino/scheduling/MyAlarm.java
+++ app/src/main/java/com/example/nickomarsellino/scheduling/MyAlarm.java
@@ -5,7 +5,9 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
 //import android.support.v4.media.app.NotificationCompat;
 import android.support.v4.app.NotificationCompat;
@@ -32,7 +34,7 @@ public class MyAlarm extends BroadcastReceiver {
 
     public void createNotification(Context context, String title, String date) {
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0 , new Intent(context,Home_Page.class), 0);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, getAlarmId(context) , new Intent(context,Home_Page.class), PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.plusdata)
@@ -46,6 +48,14 @@ public class MyAlarm extends BroadcastReceiver {
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(1, builder.build());
+        notificationManager.notify(getAlarmId(context), builder.build());
+    }
+
+    public static int getAlarmId(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        int alarmId = preferences.getInt("ALARM", 1);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("ALARM", alarmId + 1).apply();
+        return alarmId;
     }
 }
