2a04fc93f19ca43642d28f408a04774667f00c10
nicko <nickomarsellino@gmail.com>
2018-04-04 15:19:44 +0700

    Hampir

--- app/src/main/java/com/example/nickomarsellino/scheduling/MyAlarm.java
+++ app/src/main/java/com/example/nickomarsellino/scheduling/MyAlarm.java
@@ -7,7 +7,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.provider.Settings;
-import android.support.v4.media.app.NotificationCompat;
+//import android.support.v4.media.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 /**
@@ -19,16 +20,17 @@ public class MyAlarm extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-       createNotification(context, "Ini Notifikasi");
+        String title = intent.getStringExtra("A");
+        createNotification(context, title);
 
     }
 
-    private void createNotification(Context context, String s) {
+    public void createNotification(Context context, String s) {
 
-        Log.d("abcde", "test notif");
+        Log.d("abcde", s);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0 , new Intent(context,Home_Page.class), 0);
 
-        android.support.v4.app.NotificationCompat.Builder builder = new android.support.v4.app.NotificationCompat.Builder(context)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.plusdata)
                 .setContentTitle(s);
 
@@ -38,7 +40,6 @@ public class MyAlarm extends BroadcastReceiver {
         builder.setAutoCancel(true);
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
 
         notificationManager.notify(1, builder.build());
     }
